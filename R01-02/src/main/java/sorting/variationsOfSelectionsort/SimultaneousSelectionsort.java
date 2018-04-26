67a4ba6a5167d15d644e9a8b9ca5c6f8718da8f2
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-04-26 18:08:40 -0300

    Primeiro Roteiro

---
+++ R01-02/src/main/java/sorting/variationsOfSelectionsort/SimultaneousSelectionsort.java
@@ -0,0 +1,20 @@
+package sorting.variationsOfSelectionsort;
+
+import sorting.AbstractSorting;
+
+/**
+ * This algorithm applies two selection sorts simultaneously. In a same
+ * iteration, a selection sort pushes the greatest elements to the right and
+ * another selection sort pushes the smallest elements to the left. At the end
+ * of the first iteration, the smallest element is in the first position (index
+ * 0) and the greatest element is the last position (index N-1). The next
+ * iteration does the same from index 1 to index N-2. And so on. The execution
+ * continues until the array is completely ordered.
+ */
+public class SimultaneousSelectionsort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+}

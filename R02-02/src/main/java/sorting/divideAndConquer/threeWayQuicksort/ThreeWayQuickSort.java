c9b263cd6a76cb5e32b1902a59abf76b4b722b60
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-05-08 10:50:47 -0300

    Roteiro 2 feito definitivamente

--- R02-02/src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
+++ R02-02/src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
@@ -5,82 +5,73 @@ import util.Util;
 
 public class ThreeWayQuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-   /**
-    * No algoritmo de quicksort, selecionamos um elemento como pivot, particionamos
-    * o array colocando os menores a esquerda do pivot e os maiores a direita do
-    * pivot, e depois aplicamos a mesma estrategia recursivamente na particao a
-    * esquerda do pivot e na particao a direita do pivot.
-    * 
-    * Considerando um array com muitoe elementos repetidos, a estrategia do
-    * quicksort pode ser otimizada para lidar de forma mais eficiente com isso.
-    * Essa melhoria eh conhecida como quicksort tree way e consiste da seguinte
-    * ideia: - selecione o pivot e particione o array de forma que * arr[l..i]
-    * contem elementos menores que o pivot * arr[i+1..j-1] contem elementos iguais
-    * ao pivot. * arr[j..r] contem elementos maiores do que o pivot.
-    * 
-    * Obviamente, ao final do particionamento, existe necessidade apenas de ordenar
-    * as particoes contendo elementos menores e maiores do que o pivot. Isso eh
-    * feito recursivamente.
-    **/
-   @Override
-   public void sort(T[] array, int leftIndex, int rightIndex) {
+	/**
+	 * No algoritmo de quicksort, selecionamos um elemento como pivot, particionamos
+	 * o array colocando os menores a esquerda do pivot e os maiores a direita do
+	 * pivot, e depois aplicamos a mesma estrategia recursivamente na particao a
+	 * esquerda do pivot e na particao a direita do pivot.
+	 * 
+	 * Considerando um array com muitoe elementos repetidos, a estrategia do
+	 * quicksort pode ser otimizada para lidar de forma mais eficiente com isso.
+	 * Essa melhoria eh conhecida como quicksort tree way e consiste da seguinte
+	 * ideia: - selecione o pivot e particione o array de forma que * arr[l..i]
+	 * contem elementos menores que o pivot * arr[i+1..j-1] contem elementos iguais
+	 * ao pivot. * arr[j..r] contem elementos maiores do que o pivot.
+	 * 
+	 * Obviamente, ao final do particionamento, existe necessidade apenas de ordenar
+	 * as particoes contendo elementos menores e maiores do que o pivot. Isso eh
+	 * feito recursivamente.
+	 **/
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
 
-      boolean valid = inputValidation(array, leftIndex, rightIndex);
+		boolean valid = inputValidation(array, leftIndex, rightIndex);
 
-      if (valid) {
+		if (valid) {
 
-         int[] indexPivots = quick(array, leftIndex, rightIndex);
-         int indexPivot = indexPivots[0];
-         int samesPivot = indexPivots[1];
+			int begin = leftIndex;
+			int end = rightIndex;
+			int i = leftIndex + 1;
 
-         sort(array, leftIndex, indexPivot - samesPivot - 1);
-         sort(array, indexPivot + 1, rightIndex);
-      }
+			int pivotIndex = leftIndex;
+			T pivotValue = array[pivotIndex];
 
-   }
+			while (i <= end) {
 
-   private int[] quick(T[] array, int leftIndex, int rightIndex) {
+				if (array[i].compareTo(pivotValue) < 0) {
+					Util.swap(array, i, begin);
+					begin++;
+					i++;
+				} else if (pivotValue.compareTo(array[i]) < 0) {
+					Util.swap(array, i, end);
+					end--;
+				} else {
+					i++;
+				}
 
-      int begin = leftIndex + 1;
-      int end = rightIndex;
-      int same = 0;
+			}
 
-      while (begin <= end) {
-         if (array[begin].compareTo(array[leftIndex]) <= 0) {
-            begin++;
-         } else if (array[begin].compareTo(array[leftIndex]) == 0) {
-            Util.swap(array, begin, begin - 1);
-            begin++;
-            same++;
-         } else if (array[leftIndex].compareTo(array[end]) < 0)
-            end--;
-         else {
-            Util.swap(array, begin, end);
-            begin++;
-            end--;
-         }
+			sort(array, leftIndex, begin - 1);
+			sort(array, end + 1, rightIndex);
 
-      }
+		}
 
-      Util.swap(array, leftIndex, end);
-      return new int[] {end, same};
+	}
 
-   }
+	private boolean inputValidation(T[] array, int leftIndex, int rightIndex) {
 
-   private boolean inputValidation(T[] array, int leftIndex, int rightIndex) {
+		if (array == null)
+			return false;
+		if (array.length == 0)
+			return false;
+		if (leftIndex < 0 || rightIndex < 0)
+			return false;
+		if (leftIndex >= rightIndex)
+			return false;
+		if (rightIndex >= array.length)
+			return false;
 
-      if (array == null)
-         return false;
-      if (array.length == 0)
-         return false;
-      if (leftIndex < 0 || rightIndex < 0)
-         return false;
-      if (leftIndex >= rightIndex)
-         return false;
-      if (rightIndex >= array.length)
-         return false;
-
-      return true;
-   }
+		return true;
+	}
 
 }

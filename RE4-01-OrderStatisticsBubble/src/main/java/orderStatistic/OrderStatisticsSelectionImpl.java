ebfbcc14a41ade85fb511af465d0e09a49e8f333
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-05-16 20:11:41 -0300

    Implementacao do roteiro especial 04 feita

--- RE4-01-OrderStatisticsBubble/src/main/java/orderStatistic/OrderStatisticsSelectionImpl.java
+++ RE4-01-OrderStatisticsBubble/src/main/java/orderStatistic/OrderStatisticsSelectionImpl.java
@@ -3,25 +3,71 @@ package orderStatistic;
 public class OrderStatisticsSelectionImpl<T extends Comparable<T>> implements OrderStatistics<T> {
 
 	/**
-	 * Esta eh uma implementacao do calculo da estatistica de ordem seguindo a estrategia 
-	 * de usar o selection sem modificar o array original. Note que seu algoritmo vai 
-	 * apenas aplicar sucessivas vezes o selection ate encontrar a estatistica de ordem 
-	 * desejada sem modificar o array original. 
+	 * Esta eh uma implementacao do calculo da estatistica de ordem seguindo a
+	 * estrategia de usar o selection sem modificar o array original. Note que seu
+	 * algoritmo vai apenas aplicar sucessivas vezes o selection ate encontrar a
+	 * estatistica de ordem desejada sem modificar o array original.
 	 * 
-	 * Restricoes:
-	 * - Preservar o array original, ou seja, nenhuma modificacao pode ser feita no 
-	 *   array original
-	 * - Nenhum array auxiliar deve ser criado e utilizado.
-	 * - Voce nao pode encontrar a k-esima estatistica de ordem por contagem de
-	 *   elementos maiores/menores, mas sim poraplciar sucessivas selecoes.
-	 * - Caso a estatistica de ordem nao exista no array, o algoritmo deve retornar null. 
-	 * - Considerar que k varia de 1 a N 
-	 * - Sugestao: o uso de recursao ajudara sua codificacao.
+	 * Restricoes: - Preservar o array original, ou seja, nenhuma modificacao pode
+	 * ser feita no array original - Nenhum array auxiliar deve ser criado e
+	 * utilizado. - Voce nao pode encontrar a k-esima estatistica de ordem por
+	 * contagem de elementos maiores/menores, mas sim poraplciar sucessivas
+	 * selecoes. - Caso a estatistica de ordem nao exista no array, o algoritmo deve
+	 * retornar null. - Considerar que k varia de 1 a N - Sugestao: o uso de
+	 * recursao ajudara sua codificacao.
 	 */
 	@Override
 	public T getOrderStatistics(T[] array, int k) {
-		//TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		boolean valid = inputValidation(array);
+
+		if (valid) {
+
+			int maximoValue = 0;
+			int minimoValue = 0;
+			for (int i = 0; i < array.length; i++) {
+				if (array[i].compareTo(array[maximoValue]) > 0)
+					maximoValue = i;
+				if (array[i].compareTo(array[minimoValue]) < 0)
+					minimoValue = i;
+			}
+
+			return getOrderStatisticsRec(array, k, 1, minimoValue, maximoValue);
+		}
+
+		return null;
+	}
+
+	private T getOrderStatisticsRec(T[] array, int k, int qtdMinimo, int lastMinimo, int maxIndex) {
+
+		if (qtdMinimo == k)
+			return array[lastMinimo];
+
+		if (qtdMinimo == array.length)
+			return null;
+		
+		int minimo = maxIndex;
+			
+		for (int i = 0; i < array.length; i++) {
+			
+			if (array[i].compareTo(array[minimo]) < 0) {
+				if (array[i].compareTo(array[lastMinimo]) > 0)
+					minimo = i;
+			}
+		}
+		
+		return getOrderStatisticsRec(array, k, qtdMinimo + 1, minimo, maxIndex);
+
+	}
+
+	private boolean inputValidation(T[] array) {
+
+		if (array == null)
+			return false;
+		if (array.length == 0)
+			return false;
+
+		return true;
 	}
 
 }
--- RE4-01-OrderStatisticsBubble/src/test/java/sorting/test/StudentSortingTest.java
+++ RE4-01-OrderStatisticsBubble/src/test/java/sorting/test/StudentSortingTest.java
@@ -2,12 +2,11 @@ package sorting.test;
 
 import java.util.Arrays;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import sorting.AbstractSorting;
-import sorting.variationsOfBubblesort.RecursiveBubbleSort;
+import orderStatistic.OrderStatistics;
+import orderStatistic.OrderStatisticsSelectionImpl;
 
 public class StudentSortingTest {
 
@@ -15,17 +14,15 @@ public class StudentSortingTest {
 	private Integer[] vetorTamImpar;
 	private Integer[] vetorVazio = {};
 	private Integer[] vetorValoresRepetidos;
-	private Integer[] vetorValoresIguais;
 	private Integer[] vetorValoresNegativos;
 
-	public AbstractSorting<Integer> implementation;
+	public OrderStatistics<Integer> implementation;
 
 	@Before
 	public void setUp() {
-		populaVetorTamanhoPar(new Integer[] { 30, 28, 7, 29, 11, 26, 4, 22, 22, 11 });
+		populaVetorTamanhoPar(new Integer[] { 30, 28, 7, 29, 26, 4, 23, 22, 11 });
 		populaVetorTamanhoImpar(new Integer[] { 6, 41, 32, 7, 26, 4, 37, 49, 11, 18, 36 });
-		populaVetorRepetido(new Integer[] { 4, 9, 3, 4, 0, 5, 1, 4 });
-		populaVetorIgual(new Integer[] { 6, 6, 6, 6, 6, 6 });
+		populaVetorRepetido(new Integer[] {  9, 3, 4, 0, 5, 1 });
 		populaValoresNegativos(new Integer[] { 16, 6, 4, 8, 31, -17, 25, 12, -9, 7, -2, 0 });
 
 		getImplementation();
@@ -38,7 +35,7 @@ public class StudentSortingTest {
 	 */
 	private void getImplementation() {
 
-		this.implementation = new RecursiveBubbleSort<Integer>();
+		this.implementation = new OrderStatisticsSelectionImpl<Integer>();
 
 	}
 
@@ -54,10 +51,6 @@ public class StudentSortingTest {
 		this.vetorValoresRepetidos = Arrays.copyOf(arrayPadrao, arrayPadrao.length);
 	}
 
-	public void populaVetorIgual(Integer[] arrayPadrao) {
-		this.vetorValoresIguais = Arrays.copyOf(arrayPadrao, arrayPadrao.length);
-	}
-
 	private void populaValoresNegativos(Integer[] arrayPadrao) {
 		this.vetorValoresNegativos = Arrays.copyOf(arrayPadrao, arrayPadrao.length);
 
@@ -66,187 +59,41 @@ public class StudentSortingTest {
 	// FIM DOS METODOS AUXILIARES DA INICIALIZAÇÃO
 	// MÉTODOS DE TESTE
 
-	public void genericTest(Integer[] array) {
-		Integer[] copy1 = {};
-		if (array.length > 0) {
-			copy1 = Arrays.copyOf(array, array.length);
-		}
-		System.out.println(Arrays.toString(array));
-		implementation.sort(array);
-		Arrays.sort(copy1);
-
+	public void genericTest(Integer[] array, int key) {
 		
-		System.out.println(Arrays.toString(array));
-		System.out.println(Arrays.toString(copy1));
-		System.out.println("");
+		//int value2 = Arrays.binarySearch(array, key) + 1;
+		
 
-		Assert.assertArrayEquals(copy1, array);
+		System.out.println("result: " + implementation.getOrderStatistics(array, key));
 
-	}
+		//assertEquals(value1, value2);
 
-	public void delimitedTest(Integer[] array, int leftIndex, int rightIndex) {
-
-		Integer[] copy1 = {};
-		if (array.length > 0) {
-			copy1 = Arrays.copyOf(array, array.length);
-		}
-
-		implementation.sort(array, leftIndex, rightIndex);
-		Arrays.sort(copy1, leftIndex, rightIndex + 1);
-		/*
-		 * a implementação do Arrays.sort() é exclusiva com rightIndex passado, por isso
-		 * o +1.
-		 */
-
-		Assert.assertArrayEquals(copy1, array);
 	}
 
 	@Test
 	public void testSort01() {
-		genericTest(vetorTamPar);
+		genericTest(vetorTamPar, 4);
+		//
 	}
 
 	@Test
 	public void testSort02() {
-		genericTest(vetorTamImpar);
+		genericTest(vetorTamImpar, 4);
 	}
 
 	@Test
 	public void testSort03() {
-		genericTest(vetorVazio);
+		genericTest(vetorVazio, 0);
 	}
 
 	@Test
 	public void testSort04() {
-		genericTest(vetorValoresIguais);
+		genericTest(vetorValoresRepetidos, 1);
 	}
 
 	@Test
 	public void testSort05() {
-		genericTest(vetorValoresRepetidos);
-	}
-
-	@Test
-	public void testSort06() {
-
-		int leftIndex = 0;
-		int rightIndex = vetorTamPar.length - 1;
-
-		Integer[] copy1 = {};
-
-		while (leftIndex <= rightIndex) {
-
-			copy1 = Arrays.copyOf(vetorTamPar, vetorTamPar.length);
-
-			delimitedTest(copy1, leftIndex, rightIndex);
-			leftIndex++;
-			rightIndex--;
-		}
-	}
-
-	@Test
-	public void testSort07() {
-
-		int leftIndex = 0;
-		int rightIndex = vetorTamImpar.length - 1;
-
-		Integer[] copy1 = {};
-
-		while (leftIndex <= rightIndex) {
-
-			copy1 = Arrays.copyOf(vetorTamImpar, vetorTamImpar.length);
-
-			delimitedTest(copy1, leftIndex, rightIndex);
-			leftIndex++;
-			rightIndex--;
-		}
-	}
-
-	@Test
-	public void testSort08() {
-		int leftIndex = 0;
-		int rightIndex = vetorVazio.length - 1;
-
-		Integer[] copy1 = {};
-
-		while (leftIndex <= rightIndex) {
-
-			copy1 = Arrays.copyOf(vetorVazio, vetorVazio.length);
-
-			delimitedTest(copy1, leftIndex, rightIndex);
-			leftIndex++;
-			rightIndex--;
-		}
-	}
-
-	@Test
-	public void testSort09() {
-		int leftIndex = 0;
-		int rightIndex = vetorValoresIguais.length - 1;
-
-		Integer[] copy1 = {};
-
-		while (leftIndex <= rightIndex) {
-
-			copy1 = Arrays.copyOf(vetorValoresIguais, vetorValoresIguais.length);
-
-			delimitedTest(copy1, leftIndex, rightIndex);
-			leftIndex++;
-			rightIndex--;
-		}
-	}
-
-	@Test
-	public void testSort10() {
-		int leftIndex = 0;
-		int rightIndex = vetorValoresRepetidos.length - 1;
-
-		Integer[] copy1 = {};
-
-		while (leftIndex <= rightIndex) {
-
-			copy1 = Arrays.copyOf(vetorValoresRepetidos, vetorValoresRepetidos.length);
-
-			delimitedTest(copy1, leftIndex, rightIndex);
-			leftIndex++;
-			rightIndex--;
-		}
-	}
-	/*
-	@Test
-	public void testSort11() {
-
-		int quantidade = 30000;
-		Integer[] vetor = new Integer[quantidade];
-
-		for (int i = 0; i < vetor.length; i++) {
-			vetor[i] = (int) (Math.random() * quantidade);
-		}
-
-		genericTest(vetor);
-
-	}*/
-
-	@Test
-	public void testSort12() {
-		genericTest(vetorValoresNegativos);
-	}
-
-	@Test
-	public void testSort13() {
-		int leftIndex = 0;
-		int rightIndex = vetorValoresNegativos.length - 1;
-
-		Integer[] copy1 = {};
-
-		while (leftIndex <= rightIndex) {
-
-			copy1 = Arrays.copyOf(vetorValoresNegativos, vetorValoresNegativos.length);
-
-			delimitedTest(copy1, leftIndex, rightIndex);
-			leftIndex++;
-			rightIndex--;
-		}
+		genericTest(vetorValoresNegativos, 5);
 	}
 
 }

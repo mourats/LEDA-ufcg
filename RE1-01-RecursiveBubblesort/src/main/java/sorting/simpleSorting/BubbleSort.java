754f00a57db12bbdd101050fa95f2ee17e4ee922
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-05-16 21:00:53 -0300

    Add o Bubblesort para comparar as curvas geradas

---
+++ RE1-01-RecursiveBubblesort/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -0,0 +1,49 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * The bubble sort algorithm iterates over the array multiple times, pushing big
+ * elements to the right by swapping adjacent elements, until the array is
+ * sorted.
+ */
+public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+
+		boolean valid = inputValidation(array, leftIndex, rightIndex);
+
+		if (valid) {
+
+			int i = leftIndex;
+
+			while (i < rightIndex) {
+				
+				for (int j = leftIndex; j < rightIndex; j++) {
+					if (array[j].compareTo(array[j + 1]) > 0)
+						Util.swap(array, j, j + 1);
+
+				}
+				i++;
+			}
+		}
+	}
+
+	private boolean inputValidation(T[] array, int leftIndex, int rightIndex) {
+
+		if (array == null)
+			return false;
+		if (array.length == 0)
+			return false;
+		if (leftIndex < 0 || rightIndex < 0)
+			return false;
+		if (leftIndex >= rightIndex)
+			return false;
+		if (rightIndex >= array.length)
+			return false;
+
+		return true;
+	}
+}

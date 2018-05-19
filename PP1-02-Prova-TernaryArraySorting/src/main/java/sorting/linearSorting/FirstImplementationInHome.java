702d6db85f81479588f10d0ebcc667a066cb952f
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-05-19 19:19:08 -0300

    Arquivando a primeira implementação utilizando apenas o partition of quicksort

---
+++ PP1-02-Prova-TernaryArraySorting/src/main/java/sorting/linearSorting/FirstImplementationInHome.java
@@ -0,0 +1,107 @@
+package sorting.linearSorting;
+
+import util.Util;
+
+/**
+ * Primeira implementação utilizando apenas o partition of quicksort;
+ * 
+ * @author thiagomoura
+ *
+ * @param <T>
+ */
+public class FirstImplementationInHome<T extends Comparable<T>> implements TernaryArraySorting<T> {
+
+	@Override
+	public void sort(T[] ternaryArray) {
+
+		int leftIndex = 0;
+		int rightIndex = ternaryArray.length - 1;
+
+		boolean valid = inputValidation(ternaryArray, leftIndex, rightIndex);
+
+		if (valid) {
+
+			int cont = 0;
+			int maximo = leftIndex;
+			int minimo = leftIndex;
+			int medio = leftIndex;
+
+			int i = 0;
+			while (cont < 2) {
+				if (ternaryArray[i].compareTo(ternaryArray[maximo]) > 0) {
+					if (cont == 1)
+						medio = maximo;
+					else
+						maximo = i;
+
+					cont++;
+
+				} else if (ternaryArray[i].compareTo(ternaryArray[minimo]) < 0) {
+					if (cont == 1)
+						medio = minimo;
+					else
+						minimo = i;
+
+					cont++;
+				}
+				i++;
+			}
+
+			Util.swap(ternaryArray, medio, leftIndex);
+
+			T objMedio = ternaryArray[leftIndex];
+
+			int index = partition(ternaryArray, leftIndex, rightIndex);
+
+			i = 0;
+			boolean guard = true;
+			while (guard) {
+				if (objMedio.compareTo(ternaryArray[i]) > 0) {
+					Util.swap(ternaryArray, i, leftIndex);
+					guard = false;
+				}
+				i++;
+			}
+
+			partition(ternaryArray, leftIndex, index - 1);
+		}
+	}
+
+	private int partition(T[] ternaryArray, int leftIndex, int rightIndex) {
+
+		int inicio = leftIndex + 1;
+		int fim = rightIndex;
+
+		while (inicio <= fim) {
+
+			if (ternaryArray[inicio].compareTo(ternaryArray[leftIndex]) <= 0)
+				inicio++;
+			else if (ternaryArray[fim].compareTo(ternaryArray[leftIndex]) > 0)
+				fim--;
+			else {
+				Util.swap(ternaryArray, inicio, fim);
+				inicio++;
+				fim--;
+			}
+		}
+
+		Util.swap(ternaryArray, leftIndex, fim);
+		return fim;
+	}
+
+	private boolean inputValidation(T[] array, int leftIndex, int rightIndex) {
+
+		if (array == null)
+			return false;
+		if (array.length == 0)
+			return false;
+		if (leftIndex < 0 || rightIndex < 0)
+			return false;
+		if (leftIndex >= rightIndex)
+			return false;
+		if (rightIndex >= array.length)
+			return false;
+
+		return true;
+	}
+}

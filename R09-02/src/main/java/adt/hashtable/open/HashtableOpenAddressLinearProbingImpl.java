0db4fa0b2e68611a0afa6e16e39da81fbd415286
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-06-11 17:16:50 -0300

    Subindo modificações no roteiro 09

--- R09-02/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R09-02/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -2,12 +2,11 @@ package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
-		AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size,
-			HashFunctionClosedAddressMethod method) {
+	public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
 		super(size);
 		hashFunction = new HashFunctionLinearProbing<T>(size, method);
 		this.initiateInternalTable(size);
@@ -15,8 +14,29 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			
+			int cont = 0;
+			boolean guard = true;
+			int index = 0;
+			
+
+			while (guard && cont < capacity()) {
+				index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, cont);
+				if (this.table[index] == null)
+					guard = false;
+				else
+					this.COLLISIONS++;
+				
+				cont++;
+			}
+			
+			if(cont == capacity())
+				throw new HashtableOverflowException();
+			
+			this.table[index] = element;
+			this.elements++;
+		}
 	}
 
 	@Override

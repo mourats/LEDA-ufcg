c68889fa2a3441bf1134577188eda74beac5e6c6
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-07-18 00:22:04 -0300

    Iniciando implementação do roteiro 16

--- R16-02/src/main/java/adt/bt/Util.java
+++ R16-02/src/main/java/adt/bt/Util.java
@@ -4,25 +4,59 @@ import adt.bst.BSTNode;
 
 public class Util {
 
-
 	/**
 	 * A rotacao a esquerda em node deve subir e retornar seu filho a direita
+	 * 
 	 * @param node
 	 * @return
 	 */
 	public static <T extends Comparable<T>> BSTNode<T> leftRotation(BSTNode<T> node) {
-		//TODO Implemente sua rotacao a esquerda aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		BSTNode<T> pivot = (BSTNode<T>) node.getRight();
+		pivot.setParent(node.getParent());
+		node.setParent(pivot);
+		node.setRight(pivot.getLeft());
+		pivot.getLeft().setParent(node);
+		pivot.setLeft(node);
+
+		return (BSTNode<T>) pivot;
 	}
 
 	/**
 	 * A rotacao a direita em node deve subir e retornar seu filho a esquerda
+	 * 
 	 * @param node
 	 * @return
 	 */
 	public static <T extends Comparable<T>> BSTNode<T> rightRotation(BSTNode<T> node) {
-		//TODO Implemente sua rotacao a esquerda aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		BSTNode<T> pivot = (BSTNode<T>) node.getLeft();
+		pivot.setParent(node.getParent());
+		node.setParent(pivot);
+		node.setLeft(pivot.getRight());
+		pivot.getRight().setParent(node);
+		pivot.setRight(node);
+
+		return (BSTNode<T>) pivot;
+	}
+	
+	/**
+	 * Swaps the contents of two positions in an array.
+	 *
+	 * @param array
+	 *            The array to be modified, not null
+	 * @param i
+	 *            One of the target positions
+	 * @param j
+	 *            The other target position
+	 */
+	public static void swap(Object[] array, int i, int j) {
+		if (array == null)
+			throw new IllegalArgumentException();
+
+		Object temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	public static <T extends Comparable<T>> T[] makeArrayOfComparable(int size) {
